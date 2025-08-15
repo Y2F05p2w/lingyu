a39909fb20bf0c907c30774b82c4b598b82603a2
Y2F05p2w <18109503841@163.com>
2025-08-14 23:42:07 -0400

    3

--- web/db_config.php
+++ web/db_config.php
@@ -1,12 +1,11 @@
 <?php
-$servername = "db";
+$host = "localhost";
 $username = "root";
 $password = "Lifei23";
 $dbname = "target_db";
 
 // 创建数据库连接
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+$conn = new mysqli($host, $username, $password, $dbname);
 // 检查连接
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
